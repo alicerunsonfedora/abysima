a3d8d92d030d8dd0569ef204b7c1e757543c2b8d
Marquis Kurt <software@marquiskurt.net>
2021-10-14 12:27:24 -0400

    :sparkles: Add random word generator

---
+++ random_strings.py
@@ -0,0 +1,45 @@
+# random_strings.py
+# (C) 2021 Marquis Kurt.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from random import randrange, choice
+from string import ascii_lowercase
+
+
+def new_word(**kwargs) -> str:
+    """Generates a random word in the Latin alphabet.
+    
+    Kwargs:
+        min (int): The minimum number of characters the word must have. Defaults to 3.
+        max (int): The maximum number of characters the word must have. Defaults to 12.
+        syllable (str): A string of C's and V's that describes the syllabic structure of a word. Defaults to "CV"
+            (consonant, vowel).
+
+    Returns:
+        A word ranging from the minimum to the maximum lengths with the specified structure.
+    """
+    min_length = kwargs.get("min", 3)
+    max_length = kwargs.get("max", 12)
+    syllabic_structure = kwargs.get("syllable", "CV")
+    if len(syllabic_structure) > min_length:
+        min_length = len(syllabic_structure)
+    vowels = "aeiouy"
+    consonants = [c for c in ascii_lowercase if c not in vowels]
+    word_length = randrange(min_length, max_length)
+    pattern = [0 if i == "C" else 1 for i in syllabic_structure]
+
+    result = ""
+    syllable_idx = 0
+    for _ in range(word_length):
+        result += choice(vowels) if pattern[syllable_idx] else choice(consonants)
+        syllable_idx += 1
+        if syllable_idx >= len(pattern):
+            syllable_idx = 0
+    return result
+
+if __name__ == "__main__":
+    dictionary = [new_word(min=4) + "\n" for _ in range(500)]
+    with open("test_dict.txt", "w+") as dict_file:
+        dict_file.writelines(dictionary)
